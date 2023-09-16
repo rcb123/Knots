2c546590ed5a34e97aa58729ddf789d7a2b98f21
Reza Banankhah <banankha@usc.edu>
2023-09-16 12:12:20 -0700

    Fixed no activities found message

--- src/routes/(root)/activity/+page.svelte
+++ src/routes/(root)/activity/+page.svelte
@@ -8,35 +8,33 @@
 
 <h1 class="text-heading2-bold text-light-1">Activity</h1>
 
-{#if activities}
-	<section class="mt-10 flex flex-col gap-5">
-		{#if activities.length === 0}
-			<p class="!text-base-regular text-light-3">No activity yet</p>
-		{:else}
-			{#each activities as activity (activity.id)}
-				<a href={`/knot/${activity.parentId}`}>
-					<article class="flex items-center gap-2 rounded-md bg-dark-2 px-7 py-4">
-						{#if activity.author}
-							<img
-								src={activity.author.profile_image ||
-									'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='}
-								alt="user_logo"
-								width={20}
-								height={20}
-								class="rounded-full object-cover"
-							/>
-							<p class="!text-small-regular text-light-1">
-								<span class="mr-1 text-primary-500">
-									{activity.author.name}
-								</span>{' '}
-								replied to your knot
-							</p>
-						{:else}
-							Unknown author replied to your knot
-						{/if}
-					</article>
-				</a>
-			{/each}
-		{/if}
-	</section>
-{/if}
+<section class="mt-10 flex flex-col gap-5">
+	{#if !activities || activities.length === 0}
+		<p class="text-center !text-base-regular text-light-3">No activity yet</p>
+	{:else}
+		{#each activities as activity (activity.id)}
+			<a href={`/knot/${activity.parentId}`}>
+				<article class="flex items-center gap-2 rounded-md bg-dark-2 px-7 py-4">
+					{#if activity.author}
+						<img
+							src={activity.author.profile_image ||
+								'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='}
+							alt="user_logo"
+							width={20}
+							height={20}
+							class="rounded-full object-cover"
+						/>
+						<p class="!text-small-regular text-light-1">
+							<span class="mr-1 text-primary-500">
+								{activity.author.name}
+							</span>{' '}
+							replied to your knot
+						</p>
+					{:else}
+						Unknown author replied to your knot
+					{/if}
+				</article>
+			</a>
+		{/each}
+	{/if}
+</section>
